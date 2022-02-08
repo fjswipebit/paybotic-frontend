78861b62bc33c0bcd8b7ac219101a1065dee3237
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2022-02-09 02:36:58 +0800

    changes state/province field to a dropdown select in physical address section

--- src/views/cash-advance-applications/Create.vue
+++ src/views/cash-advance-applications/Create.vue
@@ -464,6 +464,7 @@
                     v-maska="'AA'"
                     v-model="form.state"
                     autocomplete="not"
+                    @change="handleStateSelect($event)"
                     class="
                       mt-1
                       focus:ring-indigo-500
@@ -687,28 +688,48 @@
                       class="block text-sm font-medium text-gray-700"
                       >State / Province</label
                     >
-                    <!-- <select class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                      <option>United States</option>
-                      <option>Canada</option>
-                      <option>Mexico</option>
-                    </select>                 -->
                     <input
-                      type="text"
-                      v-maska="'AA'"
                       required
+                      type="text"
+                      list="state"
+                      v-maska="'AA'"
                       v-model="form.physical_state"
+                      autocomplete="not"
                       class="
-                        mt-1
-                        focus:ring-indigo-500
-                        focus:border-indigo-500
-                        block
-                        w-full
-                        shadow-sm
-                        sm:text-sm
-                        border-gray-300
-                        rounded-md
-                      "
+                      mt-1
+                      focus:ring-indigo-500
+                      focus:border-indigo-500
+                      block
+                      w-full
+                      shadow-sm
+                      sm:text-sm
+                      border-gray-300
+                      rounded-md
+                    "
                     />
+                    <datalist
+                      id="state"
+                      class="
+                      mt-1
+                      focus:ring-indigo-500
+                      focus:border-indigo-500
+                      block
+                      w-full
+                      shadow-sm
+                      sm:text-sm
+                      border-gray-300
+                      rounded-md
+                    "
+                    >
+                      <option
+                        class="
+                    hidden"
+                        v-for="(state, i) in stateOptions"
+                        :key="i"
+                        :value="state"
+                      >
+                      </option>
+                    </datalist>
                   </div>
 
                   <div class="col-span-6 sm:col-span-3 lg:col-span-2">
